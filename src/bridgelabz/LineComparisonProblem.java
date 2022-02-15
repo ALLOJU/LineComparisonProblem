d9012aeb814aecd70b3cd1a061ec9ef1aa2e3c02
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-15 17:08:56 +0530

    UC3 Branch added

--- src/bridgelabz/LineComparisonProblem.java
+++ src/bridgelabz/LineComparisonProblem.java
@@ -26,27 +26,34 @@ public class LineComparisonProblem {
          return result;
          
  }
-	   public static void main(String[] args) {
-	
-Scanner scanner = new Scanner(System.in);
-System.out.println("Welcome to Compute Line Comparison program ");
-     System.out.println("Please enter values for line 1");
-     int lenthOfline_1 = getLengthOfLine(scanner);
-     
-    // System.out.println(" Length of Line is "+lenthOfline_1);
-     System.out.println("Please enter values for line 2");
-     int lenthOfline_2 = getLengthOfLine(scanner);
-    
-     String val1=String.valueOf(lenthOfline_1);
-     String val2=String.valueOf(lenthOfline_2);
-    
-     if(val1.equals(val2)){
-             System.out.println("Both lines are equal");
-     }
-     else {
-             System.out.println("Both lines are not equal");
-     }
-     scanner.close();
-
-}
+	  public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          
+          System.out.println("Please enter values for line 1");
+          int lenthOfline_1 = getLengthOfLine(scanner);
+          System.out.println("Please enter values for line 2");
+          int lenthOfline_2 = getLengthOfLine(scanner);
+         
+          
+          String val1=String.valueOf(lenthOfline_1);
+          String val2=String.valueOf(lenthOfline_2);
+          int res=comparTomethod(val1,val2);
+			
+          
+          scanner.close();
+  }
+	private static int comparTomethod(String val1, String val2) {
+		int res=val1.compareTo(val2);
+			System.out.println(res);
+			if(res==0) {
+				System.out.println("both lines are equal");
+			}
+			else if(res==1) {
+				System.out.println("Line1 is greater than Line 2");
+			}
+			else if(res==-1) {
+				System.out.println("Line 2 is greater than Line 1");
+			}
+		return res;
+	}
 }
